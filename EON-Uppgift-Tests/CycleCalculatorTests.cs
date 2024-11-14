31697b5f111b42cce5940092d222460aae5a17cb
annelie-jansson <150782755+annelie-jansson@users.noreply.github.com>
2024-11-14 19:32:03 +0100

    First assignment

---
+++ EON-Uppgift-Tests/CycleCalculatorTests.cs
@@ -0,0 +1,213 @@
+﻿using EON_Uppgift;
+
+namespace EON_Uppgift_Tests
+{
+    [TestFixture]
+    public class CycleCalculatorTests
+    {
+        [Test]
+        public void TestLongCycle()
+        {
+            List<string> cycles = new() {
+"addx 15",
+"addx -11",
+"addx 6",
+"addx -3",
+"addx 5",
+"addx -1",
+"addx -8",
+"addx 13",
+"addx 4",
+"noop",
+"addx -1",
+"addx 5",
+"addx -1",
+"addx 5",
+"addx -1",
+"addx 5",
+"addx -1",
+"addx 5",
+"addx -1",
+"addx -35",
+"addx 1",
+"addx 24",
+"addx -19",
+"addx 1",
+"addx 16",
+"addx -11",
+"noop",
+"noop",
+"addx 21",
+"addx -15",
+"noop",
+"noop",
+"addx -3",
+"addx 9",
+"addx 1",
+"addx -3",
+"addx 8",
+"addx 1",
+"addx 5",
+"noop",
+"noop",
+"noop",
+"noop",
+"noop",
+"addx -36",
+"noop",
+"addx 1",
+"addx 7",
+"noop",
+"noop",
+"noop",
+"addx 2",
+"addx 6",
+"noop",
+"noop",
+"noop",
+"noop",
+"noop",
+"addx 1",
+"noop",
+"noop",
+"addx 7",
+"addx 1",
+"noop",
+"addx -13",
+"addx 13",
+"addx 7",
+"noop",
+"addx 1",
+"addx -33",
+"noop",
+"noop",
+"noop",
+"addx 2",
+"noop",
+"noop",
+"noop",
+"addx 8",
+"noop",
+"addx -1",
+"addx 2",
+"addx 1",
+"noop",
+"addx 17",
+"addx -9",
+"addx 1",
+"addx 1",
+"addx -3",
+"addx 11",
+"noop",
+"noop",
+"addx 1",
+"noop",
+"addx 1",
+"noop",
+"noop",
+"addx -13",
+"addx -19",
+"addx 1",
+"addx 3",
+"addx 26",
+"addx -30",
+"addx 12",
+"addx -1",
+"addx 3",
+"addx 1",
+"noop",
+"noop",
+"noop",
+"addx -9",
+"addx 18",
+"addx 1",
+"addx 2",
+"noop",
+"noop",
+"addx 9",
+"noop",
+"noop",
+"noop",
+"addx -1",
+"addx 2",
+"addx -37",
+"addx 1",
+"addx 3",
+"noop",
+"addx 15",
+"addx -21",
+"addx 22",
+"addx -6",
+"addx 1",
+"noop",
+"addx 2",
+"addx 1",
+"noop",
+"addx -10",
+"noop",
+"noop",
+"addx 20",
+"addx 1",
+"addx 2",
+"addx 2",
+"addx -6",
+"addx -11",
+"noop",
+"noop",
+"noop",
+            };
+            CycleCalculator calculator = new CycleCalculator();
+
+            var result = calculator.RunCycles(cycles);
+
+            Assert.That(result, Is.EqualTo(13140));
+        }
+
+        [Test]
+        public void NoopDoesNotChangeScore() 
+        {
+            List<string> cycles = new() { "noop", "noop", "noop" };
+            CycleCalculator calculator = new CycleCalculator(17);
+
+            var result = calculator.RunCycles(cycles);
+
+            Assert.That(result, Is.EqualTo(20));
+        }
+
+
+        [Test]
+        public void AddXWillAddToScore()
+        {
+            List<string> cycles = new() { "addx 2", "noop", "noop" };
+            CycleCalculator calculator = new CycleCalculator(17);
+
+            var result = calculator.RunCycles(cycles);
+
+            Assert.That(result, Is.EqualTo(60));
+        }
+
+        [Test]
+        public void AddXWillSubtractFromScore()
+        {
+            List<string> cycles = new() { "addx -2", "noop", "noop" };
+            CycleCalculator calculator = new CycleCalculator(17);
+
+            var result = calculator.RunCycles(cycles);
+
+            Assert.That(result, Is.EqualTo(-20));
+        }
+
+
+        [Test]
+        public void AddXTakesTwoCyclesToScore()
+        {
+            List<string> cycles = new() { "noop", "addx -999", "noop" };
+            CycleCalculator calculator = new CycleCalculator(17);
+
+            var result = calculator.RunCycles(cycles);
+            
+            Assert.That(result, Is.EqualTo(20));
+        }
+
+    }
+}
---
+++ EON-Uppgift/CycleCalculator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EON_Uppgift
+{
+    public class CycleCalculator
+    {
+
+        private int _cycle = 0;
+        private int _x = 1;
+        private int _sum = 0;
+
+        /// <summary>
+        /// Only for testing purposes
+        /// </summary>
+        /// <param name="startingCycle"></param>
+        public CycleCalculator(int startingCycle)
+        {
+            _cycle = startingCycle;
+        }
+
+        public CycleCalculator()
+        {
+
+        }
+
+        public int RunCycles(List<string> signals)
+        {
+            foreach (string signal in signals)
+            {
+                if (_cycle > 220) break;
+
+                Tick(signal);
+            }
+            return _sum;
+            
+        }
+
+        private void Tick(string signal)
+        {
+            IncrementAndHandleCycle();
+
+            if (signal.Contains("noop")) return;
+
+            IncrementAndHandleCycle();
+
+            _x += GetValueFromString(signal);
+        }
+
+        private void IncrementAndHandleCycle()
+        {
+            _cycle++;
+
+            if (_cycle == 20 || _cycle == 60 || _cycle == 100 || _cycle == 140 || _cycle == 180 || _cycle == 220)
+            {
+                _sum += _x * _cycle;
+            }
+        }
+
+        private int GetValueFromString(string valueAndText)
+        {
+            var value = valueAndText.Replace("addx ", "");
+
+            if (!string.IsNullOrEmpty(value))
+            {
+                if (int.TryParse(value, out int number))
+                {
+                    return number;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
+
